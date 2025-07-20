2648ef85c512c76dd13c698a2e104760b16e3a27
Matias Montesinos <matimonte99@gmail.com>
2025-07-20 16:41:14 -0300

    Agrego pagina principal con juegos y ranking

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ const app = express();
 
 const frontendPath = path.join(__dirname, '../frontend');
 
-// Servir archivos estáticos (HTML, CSS, JS)
+
 app.use(express.static(frontendPath));
 
-// Si quieren la ruta raíz, envío el index.html
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
